ff2cbae0ba8b9ae01801cc0ef7c58050653db09e
deanbb <deanbb@github.com>
2022-08-15 17:08:17 +0900

    feat: GraphQL + MongoDB GET API 하나 추가

--- src/main/kotlin/com/h2/zzimkong/member/domain/Member.kt
+++ src/main/kotlin/com/h2/zzimkong/member/domain/Member.kt
@@ -1,13 +1,11 @@
 package com.h2.zzimkong.member.domain
 
 import com.h2.zzimkong.common.EntityBase
-import org.hibernate.annotations.DynamicInsert
-import org.hibernate.annotations.DynamicUpdate
+import org.springframework.data.mongodb.core.mapping.Document
 import javax.persistence.Entity
 
+@Document(collection = "member")
 @Entity
-@DynamicInsert
-@DynamicUpdate
 class Member constructor(
     private val name: String,
     private val password: String
--- src/main/kotlin/com/h2/zzimkong/member/service/MemberService.kt
+++
@@ -1,7 +0,0 @@
-package com.h2.zzimkong.member.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class MemberService {
-}
