41ef47146f6541bcf2f775a26c0dae01398b8b5b
deanbb <deanbb@github.com>
2022-09-04 19:58:15 +0900

    feat: Service 추가

--- src/main/kotlin/com/h2/zzimkong/member/contoller/MemberController.kt
+++ src/main/kotlin/com/h2/zzimkong/member/contoller/MemberController.kt
@@ -1,17 +1,17 @@
 package com.h2.zzimkong.member.contoller
 
-import com.h2.zzimkong.member.domain.MemberRepository
 import com.h2.zzimkong.member.domain.dto.MemberResponse
+import com.h2.zzimkong.member.service.MemberQueryService
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MemberController(
-    private val memberRepository: MemberRepository
+    private val memberService: MemberQueryService
 ) {
     @QueryMapping("findMembers")
     fun findMembers(@Argument limit: Long, @Argument offset: Long): List<MemberResponse> {
-        return memberRepository.findMembers(limit, offset)
+        return memberService.selectMembers(limit, offset)
     }
 }
