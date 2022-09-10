97d4c13dc21127b7ee8580aa3f0cca87297a91a7
HongYeol Jeon <daruken@users.noreply.github.com>
2022-09-10 21:07:40 +0900

    Develop (#6)
    * refactor: DBMS 변경. mongodb에서 MySQL로 변경
    
    * feat: Service 추가
    
    * chore: MySQL 접속정보 제거
    
    * refactor: GraphQL에 해당하는 API 함수 비동기 식으로 변경
    
    * refactor: AsyncConfig 추가 및 설정
    
    Co-authored-by: deanbb <deanbb@github.com>

--- src/main/kotlin/com/h2/zzimkong/member/domain/Member.kt
+++ src/main/kotlin/com/h2/zzimkong/member/domain/Member.kt
@@ -1,10 +1,15 @@
 package com.h2.zzimkong.member.domain
 
 import com.h2.zzimkong.common.EntityBase
+import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
 class Member constructor(
+    @Column(nullable = false)
     private val name: String,
-    private val password: String
+    @Column(nullable = false)
+    private val password: String,
+    @Column(nullable = false)
+    private val email: String
 ) : EntityBase()
