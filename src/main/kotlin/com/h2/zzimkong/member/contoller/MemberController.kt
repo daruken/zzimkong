fb27ad2127606cc3efda3bfe75ed0d8ec30b7e0a
deanbb <deanbb@github.com>
2022-09-10 21:05:43 +0900

    refactor: AsyncConfig 추가 및 설정

--- src/main/kotlin/com/h2/zzimkong/member/contoller/MemberController.kt
+++ src/main/kotlin/com/h2/zzimkong/member/contoller/MemberController.kt
@@ -2,23 +2,22 @@ package com.h2.zzimkong.member.contoller
 
 import com.h2.zzimkong.member.domain.dto.MemberResponse
 import com.h2.zzimkong.member.service.MemberQueryService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.slf4j.MDCContext
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.scheduling.annotation.Async
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.CompletableFuture
 
 @RestController
 class MemberController(
     private val memberService: MemberQueryService
 ) {
+    @Async("asyncExecutor")
     @QueryMapping("findMembers")
-    fun findMembers(@Argument limit: Long, @Argument offset: Long): List<MemberResponse> {
-        return runBlocking(Dispatchers.IO + MDCContext()) {
-            val result = async { memberService.selectMembers(limit, offset) }
-            result.await()
+    fun findMembers(@Argument limit: Long, @Argument offset: Long): CompletableFuture<List<MemberResponse>> {
+        return runBlocking {
+            CompletableFuture.completedFuture(memberService.selectMembers(limit, offset))
         }
     }
 }
