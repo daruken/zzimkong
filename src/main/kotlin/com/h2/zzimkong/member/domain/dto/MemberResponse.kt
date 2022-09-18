3116d17661a013d8b3141bb603e746fc708bc1e4
daruken <dj.hongten@gmail.com>
2022-09-18 16:26:58 +0900

    feat: Member 도메인에 email 프로퍼티 추가
    Signed-off-by: deanbb <deanbb@github.com>

--- src/main/kotlin/com/h2/zzimkong/member/domain/dto/MemberResponse.kt
+++ src/main/kotlin/com/h2/zzimkong/member/domain/dto/MemberResponse.kt
@@ -3,5 +3,6 @@ package com.h2.zzimkong.member.domain.dto
 import com.querydsl.core.annotations.QueryProjection
 
 data class MemberResponse @QueryProjection constructor(
-    val name: String
+    val name: String,
+    val email: String
 )
